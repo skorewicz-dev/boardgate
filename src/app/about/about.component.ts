bbdfc61abdb8d209dbbd4f7ae1b1f436e8d450b6
Dawid Skorewicz <skorewicz.dev@gmail.com>
2017-09-13 15:45:40 +0200

    Height scaling adjusted

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['../welcome/welcome.component.css']
 })
 export class AboutComponent implements OnInit {
 
