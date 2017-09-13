8de4c3f40a7b2104b41c4b854d5914d28f94e712
Dawid Skorewicz <skorewicz.dev@gmail.com>
2017-09-13 15:32:40 +0200

    About added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LobbyComponent } from './lobby/lobby.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LobbyComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     ConnectionModule,
     RouterModule.forRoot([
       {path: 'lobby', component: LobbyComponent },
+      {path: 'about', component: AboutComponent },
       {path: 'welcome', component: WelcomeComponent },
       {path: '', redirectTo: '/welcome', pathMatch: 'full'}
     ])
