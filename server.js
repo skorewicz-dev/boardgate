5043bdea74e07283b393c5ea2b85204d5021b4c8
Dawid Skorewicz <skorewicz.dev@gmail.com>
2017-09-14 19:54:43 +0200

    Testing Maciej 2

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/maciej'));
 
-app.get('/maciej', function(req,res){
+app.get('/*', function(req,res){
     res.sendFile(__dirname + '/maciej/index.html');
 })
 
 // Last get to handle PathLocationStrategy
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res){
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 app.listen(process.env.PORT || 8080);
