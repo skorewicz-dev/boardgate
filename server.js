29acb9dcb0e6a771567b0d4d1e4f9e0959049f28
Dawid Skorewicz <skorewicz.dev@gmail.com>
2017-09-16 13:41:58 +0200

    Going back to pet project

--- server.js
+++ server.js
@@ -5,29 +5,29 @@ const fs = require('fs');
 
 
 
-app.use(express.static(__dirname + '/maciej'));
+// app.use(express.static(__dirname + '/maciej'));
 
-app.get('/gfx/militaria/', function(req, res){
-    fs.readdir('./maciej/gfx/militaria/', (err, files) => {
-          res.send(files);
-      });
-});
+// app.get('/gfx/militaria/', function(req, res){
+//     fs.readdir('./maciej/gfx/militaria/', (err, files) => {
+//           res.send(files);
+//       });
+// });
 
-app.get('/gfx/motoryzacja/', function(req, res){
-    fs.readdir('./maciej/gfx/motoryzacja/', (err, files) => {
-          res.send(files);
-      });
-})
-app.get('/maciej/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/maciej/index.html'));
-});
+// app.get('/gfx/motoryzacja/', function(req, res){
+//     fs.readdir('./maciej/gfx/motoryzacja/', (err, files) => {
+//           res.send(files);
+//       });
+// })
+// app.get('/maciej/*', function(req, res){
+//     res.sendFile(path.join(__dirname + '/maciej/index.html'));
+// });
 
 // Last get to handle PathLocationStrategy
 
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-// app.get('/*', function(req, res){
-//     res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(process.env.PORT || 8080);
